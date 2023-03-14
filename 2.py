3cf5b7a1d266c181a8ebf8b1adf2f01d76adb278
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-03-14 22:23:51 +0900

    3/14

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
-# 나머지 구하기
+# 몫 구하기
 
 def solution(num1, num2):
-    answer = num1 % num2
+    answer = int(num1 / num2)
     return answer
 
-print(solution(3,2))
 print(solution(10,5))
+print(solution(7,2))
