87daaf8a1dcdb875e8dd674efb6da7d17c217a12
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-03-16 00:26:37 +0900

    3/16

---
+++ 14.py
@@ -0,0 +1,12 @@
+# def solution(array):
+#     array.sort()
+#     middle = round(len(array)/2)
+#     answer = array[middle]
+#     return answer
+
+array = [9, -1, 0]
+array.sort()
+print(array)
+middle = int(len(array)/2)
+print(middle)
+print(array[middle])
--- 3.py
+++ 3.py
@@ -1,6 +1,8 @@
+# 두 수의 곱
+
 def solution(num1, num2):
-    answer = int(num1 / num2)
+    answer = num1 * num2
     return answer
 
-print(solution(10, 5))
-print(solution(7,2))
+print(solution(3,4))
+print(solution(27,19))
