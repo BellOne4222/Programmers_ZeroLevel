99a591ca83e347177571313150febde268710ad6
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-18 20:16:13 +0900

    8/18

--- 24(복습하기).py
+++ 24(복습하기).py
@@ -1,11 +1,7 @@
 # 문자열 뒤집기
 
 def solution(my_string):
-    answer = []
-    i = str(i)
-    for i in my_string:
-        answer.append(i)
-    answer.reverse()
+    answer = list(my_string) # 문자열을 리스트로 변환
+    answer.reverse() # 리스트 뒤집기
+    answer = ''.join(answer) # ''.join을 사용하여 리스트 안의 요소들을 합치기
     return answer
-
-print(solution("jaron"))
